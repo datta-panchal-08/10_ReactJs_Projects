ce4323ff12622122493b88308225ef2a4b8cbdbb
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-23 09:44:28 +0530

    Modified Button Component

--- project-01/src/components/Button.jsx
+++ project-01/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
      const {icon ,text,isOutline} = props;
   return (
     <div>
-        <button  className = {`px-[2vw] flex items-center ${isOutline ? "bg-white mt-4 mb-4 font-semibold text-black border-solid w-[36.6vw] border-[.1vw] border-zinc-700 ":""} justify-center gap-[1vw]  py-[.6vw] bg-black text-white rounded`}>
+        <button  className = {`px-[2vw] flex items-center ${isOutline ? "bg-zinc-100 mt-4 mb-4 font-semibold text-red-700 border-solid w-[36.6vw] border-[.1vw] border-zinc-700 ":""} justify-center gap-[1vw]  py-[.6vw] bg-black text-white rounded`}>
           
           {icon}
           {text}
